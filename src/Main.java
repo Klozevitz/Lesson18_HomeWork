cbe47a7e2a4c91a8597563c540f97ba795222aca
Klozevitz <Klozevitz@gmail.com>
2022-04-18 23:20:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arrayTask1234 = new int[] {12, 43, 100, 4, 24, 6, 63};
+        String[] arrayOfStringsTask5 = {"tube", "youtube", "alabama", "africa",
+                "antarctica", "arctica", "rambler"};
+
+        /*Node head1 = new Node(arrayTask1234[0]);                                 //Task1
+        List list1 = new List(head1);
+        for (int i = 1; i < arrayTask1234.length; i++) {
+            list1.push(arrayTask1234[i]);
+        }
+        System.out.println(HomeWork.task1(list1));*/
+
+        /*Node head4 = new Node(arrayTask1234[0]);                                 //Task4
+        List list4 = new List(head4);
+        for (int i = 1; i < arrayTask1234.length; i++) {
+            list4.push(arrayTask1234[i]);
+        }
+        list4 = HomeWork.task4(list4);
+        list4.printList();
+        System.out.println();*/
+
+        /*Node head5 = new Node(arrayOfStringsTask5[0]);                             //Task5
+        List list5 = new List(head5);
+        for (int i = 1; i < arrayOfStringsTask5.length; i++) {
+            list5.push(arrayOfStringsTask5[i]);
+        }
+        System.out.print("В массиве ");
+        list5.printList();
+        System.out.println(HomeWork.task5(list5) + " слов начинаются и заканчиваются на одну букву.");*/
+
+    }
+}
