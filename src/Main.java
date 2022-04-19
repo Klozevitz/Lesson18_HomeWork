ba796ccfe29fc6dca62b816dd23565a08251074d
Klozevitz <Klozevitz@gmail.com>
2022-04-19 08:35:37 +0300

    Task1-6 update, list(Object[])

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,34 @@ public class Main {
         Object[] arrayTask1234 = new Object[] {12, 43, 100, 4, 24, 6, 63};
         String[] arrayOfStringsTask5 = {"tube", "youtube", "alabama", "africa",
                 "antarctica", "arctica", "rambler"};
+        String[] arrayOfStrings1Task6 = {"tube", "elf", "fusion", "nail", "lock", "key", "yellow"};
+        String[] arrayOfStrings2Task6 = {"tube", "elf", "ghost", "nail", "lock", "key", "yellow"};
 
-        System.out.println("Task1");
-        List list1 = new List(arrayTask1234);                                   //Task1
+
+        System.out.println("Task1");                                            //Task1
+        List list1 = new List(arrayTask1234);
         System.out.print("Среднее арифметическое элементов списка ");
         list1.printList();
         System.out.println(" - " + HomeWork.task1(list1) + "\n");
 
-        System.out.println("Task2");
-        List list2 = new List(arrayTask1234);                                   //Task2
+        System.out.println("Task2");                                            //Task2
+        List list2 = new List(arrayTask1234);
         list2.printList();
         System.out.println();
         HomeWork.task2(list2);
         list2.printList();
         System.out.println("\n");
 
-        System.out.println("Task3");
-        List list3 = new List(arrayTask1234);                                   //Task3
+        System.out.println("Task3");                                            //Task3
+        List list3 = new List(arrayTask1234);
         list3.printList();
         System.out.println();
         HomeWork.task3(list3);
         list3.printList();
         System.out.println("\n");
 
-        System.out.println("Task4");
-        List list4 = new List(arrayTask1234);                                   //Task4
+        System.out.println("Task4");                                            //Task4
+        List list4 = new List(arrayTask1234);
         list4.printList();
         System.out.println();
         HomeWork.task4(list4);
@@ -39,6 +42,13 @@ public class Main {
         System.out.print("В массиве ");
         list5.printList();
         System.out.println(" " + HomeWork.task5(list5) + " слов начинаются и заканчиваются на одну букву.");
+        System.out.println();
 
+        System.out.println("Task6");
+        List list16 = new List(arrayOfStrings1Task6);
+        List list26 = new List(arrayOfStrings2Task6);
+        list16.printList();
+        System.out.println();
+        list26.printList();
     }
 }
